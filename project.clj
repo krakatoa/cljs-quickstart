414527560accf9a7b329a59df5c918038f33f263
Fernando Alonso <krakatoa1987@gmail.com>
2017-02-17 16:24:09 -0300

    Basic cljsbuild working

--- project.clj
+++ project.clj
@@ -3,4 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :dependencies [ [org.clojure/clojure "1.8.0"]
+                  [org.clojure/clojurescript "1.9.473"]]
+
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src-cljs"]
+        :compiler {
+          :main 'hello-world.core
+          :output-to "target/main.js"
+          :optimizations :whitespace
+          :pretty-print true}}]})
---
+++ src-cljs/cljs_quickstart/core.cljs
@@ -0,0 +1,6 @@
+(ns cljs-quickstart.core)
+
+(enable-console-print!)
+
+;(println "Hello from ClojureScript!")
+(js/alert "Hello from ClojureScript!")
