6172e22c708329a6d448f8c43e133a336ac5a26f
Fernando Alonso <krakatoa1987@gmail.com>
2017-02-21 16:58:33 -0300

    Om basics

--- project.clj
+++ project.clj
@@ -5,9 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel "0.5.8"]]
-  :clean-targets [:target-path "out"]
+
+  :clean-targets ["out" "resources/public/js"]
+
   :dependencies [ [org.clojure/clojure "1.8.0"]
-                  [org.clojure/clojurescript "1.9.473"]]
+                  [org.clojure/clojurescript "1.9.473"]
+                  [org.omcljs/om "1.0.0-alpha46"]]
 
   :cljsbuild {
     :builds [{
@@ -16,6 +19,8 @@
         :figwheel true
         :compiler {
           :main "cljs-quickstart.core"; 'hello-world.core
-          :output-to "target/main.js"
+          :asset-path "js/out"
+          :output-to "resources/public/js/main.js"
+          :output-dir "resources/public/js/out"
           :optimizations :none ;:whitespace
           :pretty-print true}}]})
--- src-cljs/cljs_quickstart/core.cljs
+++ src-cljs/cljs_quickstart/core.cljs
@@ -1,6 +1,20 @@
-(ns cljs-quickstart.core)
+(ns cljs-quickstart.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
 (println "Hello from Sarasa!")
 ;(js/alert "Hello from ClojureScript!")
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(om/root
+  (fn [data owner]
+    (reify om/IRender
+      (render [_]
+        (dom/div nil
+                 (dom/p nil (:text data))
+                 (dom/h3 nil "Edit this and watch it change!")))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
