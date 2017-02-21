639182667f37e950666a9f114b2c1555a145356f
Fernando Alonso <krakatoa1987@gmail.com>
2017-02-21 17:41:55 -0300

    Contacts example

--- src-cljs/cljs_quickstart/core.cljs
+++ src-cljs/cljs_quickstart/core.cljs
@@ -7,14 +7,46 @@
 (println "Hello from Sarasa!")
 ;(js/alert "Hello from ClojureScript!")
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {  :text "Hello world!"
+                            :list ["Test" "Tost"]
+                            :contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h2 nil "Contact list")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts data)))))))
 
 (om/root
   (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/div nil
-                 (dom/p nil (:text data))
-                 (dom/h3 nil "Edit this and watch it change!")))))
+    (om/component
+      (apply dom/ul #js {:className "animals"}
+        (map (fn [text] (dom/li nil text)) (:list data)))))
   app-state
   {:target (. js/document (getElementById "app"))})
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
